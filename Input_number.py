33ff6421617ee70dcbf7cb3f8fe274f2944e67ee
Mrocha4100 <rochamarco61@gmail.com>
2021-08-26 12:12:26 -0400

    changed debug 2

---
+++ Input_number.py
@@ -0,0 +1,5 @@
+#This program will get a number from a user and print it to the screen.
+num + int(input("Please give me a number: "))
+num1 = num + num
+print("Your number is  " + str(num1))
+
---
+++ convert.py
@@ -0,0 +1,3 @@
+num = int(input("convert F to celsius "))
+num1 = (num - 32) / 1.8
+print(str(num1))
